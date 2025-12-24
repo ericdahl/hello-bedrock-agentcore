3fd6588ee5ae2c8ee60047dc0cc3b34d5c005387
Eric Dahl <eric.c.dahl@gmail.com>
2025-12-24 10:27:34 -0800

    init tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Name       = "hello-bedrock-agentcore"
+      Repository = "https://github.com/ericdahl/hello-bedrock-agentcore"
+    }
+  }
+}
+
+data "aws_default_tags" "default" {}
+
+
+locals {
+  name = data.aws_default_tags.default.tags["Name"]
+}
