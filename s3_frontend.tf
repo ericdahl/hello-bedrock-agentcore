b8f2fd5e03604974c6e29734097b0e1b2291ae62
Eric Dahl <eric.c.dahl@gmail.com>
2025-12-25 10:45:33 -0800

    refactor S3 TF

--- s3_frontend.tf
+++ s3_frontend.tf
@@ -1,25 +1,3 @@
-# Data source bucket for product catalog
-resource "aws_s3_bucket" "data_source" {
-  bucket        = "${local.name_prefix}-data-source"
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_versioning" "data_source" {
-  bucket = aws_s3_bucket.data_source.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "data_source" {
-  bucket = aws_s3_bucket.data_source.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
 # Frontend hosting bucket
 resource "aws_s3_bucket" "frontend" {
   bucket        = "${local.name_prefix}-frontend"
